ac91d1b0efb393a6741327f74d1aaa9d1bb341e3
Joshua Morales <joshuapmorales@gmail.com>
2020-12-22 02:13:42 +0000

    Automatically backed up by Learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -6,6 +6,7 @@ def prying_into_the_method
 	puts "We're about to stop because of pry!"
 	my_name = "Joshua Paul Morales"
 	binding.pry
+	your_name = "Timtohy"
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
 	puts this_variable_hasnt_been_interpreted_yet
 end
